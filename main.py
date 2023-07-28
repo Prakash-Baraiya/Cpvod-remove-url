ce244744971bbd8bebba254ca2bfc74c8a911685
Prakash-Baraiya <133520032+Prakash-Baraiya@users.noreply.github.com>
2023-07-28 11:18:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,70 +1,29 @@
+import requests
 import os
-import re
-import logging
-from telegram import Update
-from telegram.constants import ParseMode
-from telegram.ext import Updater, CommandHandler, MessageHandler, CallbackContext
-from telegram.ext.filters import Filters
+import telebot
 
-# Rest of the code remains the same...
+bot = telebot.TeleBot('6488165968:AAFyogItsIQm2VEsk_GWRsZAXf3ZNij-t6s')
 
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, 'Send me a txt or .txt file contains urls in name:url format')
 
-# Rest of the code remains the same...
+@bot.message_handler(content_types=['text'])
+def handle_file(message):
+    file_name = message.document.file_name
+    if file_name.endswith('.txt'):
+        file_content = message.document.file_data
+        lines = file_content.decode('utf-8').splitlines()
+        new_lines = []
+        for i, line in enumerate(lines):
+            if 'cpvod.testbook' not in line:
+                new_lines.append(line)
+            else:
+                if i < len(lines) - 1:
+                    new_lines.append(lines[i+1])
+        output_file_name = file_name.split('.')[0] + '_processed.txt'
+        with open(output_file_name, 'w', encoding='utf-8') as f:
+            f.write('\n'.join(new_lines))
+        bot.send_document(message.chat.id, open(output_file_name, 'rb'))
 
-# Set up logging
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
-logger = logging.getLogger(__name__)
-
-# Function to handle the /start command
-def start(update: Update, _: CallbackContext) -> None:
-    update.message.reply_text(
-        "Welcome! Please send a .txt file containing URLs in name:url format."
-    )
-
-# Function to handle file upload
-def handle_file(update: Update, _: CallbackContext) -> None:
-    file = update.message.document
-    if file.mime_type != 'text/plain':
-        update.message.reply_text("Please upload a .txt file.")
-        return
-    
-    file_name = f"{file.file_id}.txt"
-    file_path = f"{file_name}"
-    file.get_file().download(file_path)
-    
-    # Process the file to remove unwanted URLs
-    with open(file_path, 'r') as f:
-        lines = f.readlines()
-    
-    filtered_lines = [line for line in lines if not re.search(r'\bcpvod\.testbook\b', line)]
-    
-    # Save the filtered content to a new file
-    output_file_name = f"{file.file_id}_filtered.txt"
-    output_file_path = f"{output_file_name}"
-    
-    with open(output_file_path, 'w') as f:
-        f.writelines(filtered_lines)
-    
-    # Send the output file to the user
-    update.message.reply_document(open(output_file_path, 'rb'))
-    
-    # Clean up: delete the temporary files
-    os.remove(file_path)
-    os.remove(output_file_path)
-
-# Main function to start the bot
-def main() -> None:
-    # Replace 'YOUR_TELEGRAM_BOT_TOKEN' with your actual bot token
-    updater = Updater("6488165968:AAFyogItsIQm2VEsk_GWRsZAXf3ZNij-t6s")
-    
-    dispatcher = updater.dispatcher
-    
-    # Handlers
-    dispatcher.add_handler(CommandHandler("start", start))
-    dispatcher.add_handler(MessageHandler(Filters.document.mime_type("text/plain"), handle_file))
-    
-    updater.start_polling()
-    updater.idle()
-
-if __name__ == '__main__':
-    main()
+bot.polling()
