0b32fb30c3a14064954a88d4db5ad2d12d04a683
Prakash-Baraiya <133520032+Prakash-Baraiya@users.noreply.github.com>
2023-07-28 12:00:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,46 @@
-import requests
 import os
-import telebot
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import re
 
-bot = telebot.TeleBot('6488165968:AAFyogItsIQm2VEsk_GWRsZAXf3ZNij-t6s')
+# Replace 'YOUR_BOT_TOKEN' with your actual bot token obtained from BotFather on Telegram
+bot_token = '6488165968:AAFyogItsIQm2VEsk_GWRsZAXf3ZNij-t6s'
 
-@bot.message_handler(commands=['start'])
-def start(message):
-    bot.send_message(message.chat.id, 'Send me a txt or .txt file contains urls in name:url format')
+def start(update, context):
+    update.message.reply_text("Please send a .txt file containing name:url pairs.")
 
-@bot.message_handler(content_types=['text'])
-def handle_file(message):
-    file_name = message.document.file_name
-    if file_name.endswith('.txt'):
-        file_content = message.document.file_data
-        lines = file_content.decode('utf-8').splitlines()
-        new_lines = []
-        for i, line in enumerate(lines):
-            if 'cpvod.testbook' not in line:
-                new_lines.append(line)
-            else:
-                if i < len(lines) - 1:
-                    new_lines.append(lines[i+1])
-        output_file_name = file_name.split('.')[0] + '_processed.txt'
-        with open(output_file_name, 'w', encoding='utf-8') as f:
-            f.write('\n'.join(new_lines))
-        bot.send_document(message.chat.id, open(output_file_name, 'rb'))
+def process_txt_file(update, context):
+    file = update.message.document.get_file()
+    file.download('input.txt')
+    with open('input.txt', 'r') as infile:
+        lines = infile.readlines()
 
-bot.polling()
+    # Filter out lines containing 'cpvod.testbook'
+    filtered_lines = [line for line in lines if 'cpvod.testbook' not in line]
+
+    # Remove empty lines
+    filtered_lines = [line.strip() for line in filtered_lines if line.strip()]
+
+    # Prepare the corrected content
+    corrected_content = '\n'.join(filtered_lines)
+
+    # Write the corrected content to a new file
+    with open('output.txt', 'w') as outfile:
+        outfile.write(corrected_content)
+
+    # Send the corrected file to the user
+    context.bot.send_document(chat_id=update.effective_chat.id, document=open('output.txt', 'rb'))
+    os.remove('input.txt')
+    os.remove('output.txt')
+
+def main():
+    updater = Updater(bot_token, use_context=True)
+    dp = updater.dispatcher
+
+    dp.add_handler(CommandHandler('start', start))
+    dp.add_handler(MessageHandler(Filters.document.file_extension('txt'), process_txt_file))
+
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
